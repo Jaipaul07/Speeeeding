1f839908b51f0f6748314d20bb5670113c013d04
Jaipaul Mann <j.mann12@EDU.epsb.ca>
2022-11-04 14:16:44 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-// Caught Speeding CYU Asgn
+// Input
+let subject = +promt("Enter a school subject");
+let Color = +promt("Enter a color");
+let Noun = +promt("Enter a noun");
+ 
+// Process
+
+let 
+
+// Output
+alert(msg);
+
