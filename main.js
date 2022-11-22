70aadc4a27240e9b5b3286bef9e0e43ed4eb4fac
Jaipaul Mann <j.mann12@EDU.epsb.ca>
2022-11-22 13:37:31 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
-// Input
-let bsq = +promt("Enter a color");
-let asq= +promt("Enter a noun");
- 
-// Process
+// Intput 
+let vel = prompt("What is you level");
+alert(`${vel}`); 
 
-let math = +promt("${asq} * ${asq} + ${bsq} * ${bsq}");
-
-// Output
-alert(math);
-
+if (vel <=49) {
+    alert("you will not go to the next level");
+} else if (vel <=69) {
+    alert("The next level may be difficult");
+} else if (vel <=89) {
+    alert("You should go to the next level");
+} else {
+ alert("You will go to the next level");
+}
