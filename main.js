f4aa0a2169540b2c9956b74495b876ffdc0bcf31
Jaipaul Mann <j.mann12@EDU.epsb.ca>
2022-11-28 13:47:14 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
-let num = prompt("provide balence");
-let x = num * 0.021;
-if (num <= 10) {
-    alert(`${num}`);
-} else {
-    alert(`$${x}`);
+let num = 1;
+
+while (num <=5) {
+    console.log(num);
+    num++;
 }
+
+
+
+
