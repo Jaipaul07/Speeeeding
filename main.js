60120d7d97fce2f0af9a2aefc46c77b57eb5631b
Jaipaul Mann <j.mann12@EDU.epsb.ca>
2022-12-09 13:35:49 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
- alert("hi");
+let calcBtn = document.getElementById("calc-btn");
+let sumOut = document.getElementById("sum-out");
+
+calcBtn.addEventListener("click, calcSum");
+
+function calc() {
+    // let total = 0;
+    // for (let n = 1; n <= 5; n++) {
+       // total = total + n;
+// }
+let total = 1000 * 1001 / 2;
+sumOut.innerHTML = total;
+} 
